4e9f7474a1c5bcbc11a93100f96711af2f04743f
Heath Volmer <hvolmer@pepperdash.com>
2016-11-28 14:07:53 -0700

    Changed debug class to write to file rather than use datastore

---
+++ Pepperdash Core/Pepperdash Core/Debug/DebugContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash_Core.Debug
+{
+	public class DebugContext
+	{
+		public string Name { get; private set; }
+
+		public int Level { get; private set; }
+
+		public List<string> Keys { get; private set; }
+
+
+
+		public DebugContext(string name)
+		{
+
+		}
+
+
+	}
+}
