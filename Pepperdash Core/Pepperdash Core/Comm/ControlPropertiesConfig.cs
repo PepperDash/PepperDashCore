0e27ef3f9c796813600c1f663f1346312dacefc1
Heath Volmer <hvolmer@pepperdash.com>
2017-02-15 16:32:36 -0700

    Moved in eControlMethods and a portion of ControlPropertiesConfig from Essentials

---
+++ Pepperdash Core/Pepperdash Core/Comm/ControlPropertiesConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Newtonsoft.Json;
+
+namespace PepperDash.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ControlPropertiesConfig
+    {
+        public eControlMethod Method { get; set; }
+
+        public string ControlPortDevKey { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] // In case "null" is present in config on this value
+        public uint ControlPortNumber { get; set; }
+
+        public TcpSshPropertiesConfig TcpSshProperties { get; set; }
+
+        public string IrFile { get; set; }
+
+        //public ComPortConfig ComParams { get; set; }
+
+        //[JsonConverter(typeof(ComSpecJsonConverter))]
+        //public ComPort.ComPortSpec ComParams { get; set; }
+
+        public string IpId { get; set; }
+
+        [JsonIgnore]
+        public uint IpIdInt { get { return Convert.ToUInt32(IpId, 16); } }
+
+        public char EndOfLineChar { get; set; }
+
+        /// <summary>
+        /// Defaults to Environment.NewLine;
+        /// </summary>
+        public string EndOfLineString { get; set; }
+
+        public string DeviceReadyResponsePattern { get; set; }
+
+        public ControlPropertiesConfig()
+        {
+            EndOfLineString = CrestronEnvironment.NewLine;
+        }
+    }
+}
---
+++ Pepperdash Core/Pepperdash Core/Comm/eControlMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash.Core
+{
+    public enum eControlMethod
+    {
+        None = 0, Com, IpId, IR, Ssh, Tcpip, Telnet
+    }
+}
