31d0d1222a0aee2be856563121926e755076c42a
Joshua Gutenplan <jgutenplan@pepperdash.com>
2017-03-18 10:15:01 -0700

    Just removed the commented out parts fromt the event args.cs before updating.

--- Pepperdash Core/Pepperdash Core/Comm/EventArgs.cs
+++ Pepperdash Core/Pepperdash Core/Comm/EventArgs.cs
@@ -24,40 +24,6 @@ namespace PepperDash.Core
     }
     #endregion
 
-    //#region DynamicTCPServerStateChangedEventArgs
-    //public delegate void DynamicTCPServerStateChangedEventDelegate(object server);
-
-    //public class DynamicTCPServerStateChangedEventArgs : EventArgs
-    //{
-    //    public bool Secure { get; private set; }
-    //    public object Server { get; private set; }
-
-    //    public DynamicTCPServerStateChangedEventArgs() { }
-
-    //    public DynamicTCPServerStateChangedEventArgs(object server, bool secure)
-    //    {
-    //        Secure = secure;
-    //        Server = server;
-    //    }
-    //}
-    //#endregion
-
-    //#region DynamicTCPServerSocketStatusChangeEventArgs
-    //public delegate void DynamicTCPServerSocketStatusChangeEventDelegate(object server);
-
-    //public class DynamicTCPServerSocketStatusChangeEventArgs : EventArgs
-    //{        
-    //    public bool Secure { get; private set; }
-    //    public object Server { get; private set; }
-
-    //    public DynamicTCPServerSocketStatusChangeEventArgs() { }
-
-    //    public DynamicTCPServerSocketStatusChangeEventArgs(object server, bool secure)
-    //    {
-    //        Secure = secure;
-    //        Server = server;
-    //    }
-    //}
-    //#endregion
+   
 
 }
