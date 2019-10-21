c9e0879bdd890d868656b300e6ef4e0ec8f08c08
Neil Dorin <ndorin@pepperdash.com>
2019-10-21 16:03:42 -0600

    corrects Assembly version

--- Pepperdash Core/Pepperdash Core/Properties/AssemblyInfo.cs
+++ Pepperdash Core/Pepperdash Core/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pepperdash_Core")]
 [assembly: AssemblyCopyright("Copyright © PepperDash 2019")]
-[assembly: AssemblyVersion("1.0.26.*")]
+[assembly: AssemblyVersion("1.0.0.*")]
