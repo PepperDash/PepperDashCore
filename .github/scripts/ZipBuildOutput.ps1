82418946e1feb693d686c8bc54365fced2534655
Neil Dorin <ndorin@pepperdash.com>
2020-03-18 17:50:44 -0600

    Adds git tags command back and updates ZipBuildOutput to include .dll

--- .github/scripts/ZipBuildOutput.ps1
+++ .github/scripts/ZipBuildOutput.ps1
@@ -2,7 +2,7 @@ $destination = "$($Env:GITHUB_WORKSPACE)\output"
 New-Item -ItemType Directory -Force -Path ($destination)
 Get-ChildItem ($destination)
 $exclusions = @(git submodule foreach --quiet 'echo $name')
-Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include @("*.clz", "*.cpz", "*.cplz") | ForEach-Object {
+Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include @("*.clz", "*.cpz", "*.cplz", "$($Env:GITHUB_WORKSPACE).dll") | ForEach-Object {
   $allowed = $true;
   foreach ($exclude in $exclusions) {
     if ((Split-Path $_.FullName -Parent).contains("$($exclude)")) {
@@ -17,6 +17,8 @@ Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include @("*.clz", "*.c
 } | Copy-Item -Destination ($destination)
 Get-ChildItem "$($Env:GITHUB_WORKSPACE)\output" -include @("*.cpz", "*.clz", "*.cplz") | ForEach-Object {  
   $filenames = @($_ -replace "cpz|clz|cplz", "dll", $_ -replace "cpz|clz|cplz", "xml")
+  Write-Host "Filenames:"
+  Write-Host $filenames
   if ($filenames.length -gt 0) {
     Get-ChildItem -Recurse -Path "$($Env:GITHUB_WORKSPACE)" -include $filenames | Copy-Item -Destination ($destination)
   }
