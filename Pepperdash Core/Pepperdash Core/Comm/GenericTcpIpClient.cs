f89c75debc96e2490f079f7d89a40dea3a2ef69d
Jason Alborough <jalborough@pepperdash.com>
2020-06-09 20:44:52 -0400

    #28 Fixes issue where GenericTxpIpClient would attempt to reconnect when it was disconnected on a shutdown or program stop.

--- Pepperdash Core/Pepperdash Core/Comm/GenericTcpIpClient.cs
+++ Pepperdash Core/Pepperdash Core/Comm/GenericTcpIpClient.cs
@@ -224,7 +224,7 @@ namespace PepperDash.Core
             if (programEventType == eProgramStatusEventType.Stopping)
             {
                 Debug.Console(1, this, "Program stopping. Closing connection");
-                DisconnectClient();
+				Disconnect();
             }
         }
 
@@ -363,8 +363,8 @@ namespace PepperDash.Core
 		{
 			var bytes = Encoding.GetEncoding(28591).GetBytes(text);
 			// Check debug level before processing byte array
-			//if (Debug.Level == 2)
-			//    Debug.Console(2, this, "Sending {0} bytes: '{1}'", bytes.Length, ComTextHelper.GetEscapedText(bytes));
+			
+			Debug.Console(2, this, "Sending {0} bytes: '{1}'", bytes.Length, ComTextHelper.GetEscapedText(bytes));
             if(Client != null)
 			    Client.SendData(bytes, bytes.Length);
 		}
@@ -388,8 +388,8 @@ namespace PepperDash.Core
         /// <param name="bytes"></param>
 		public void SendBytes(byte[] bytes)
 		{
-			//if (Debug.Level == 2)
-			//    Debug.Console(2, this, "Sending {0} bytes: '{1}'", bytes.Length, ComTextHelper.GetEscapedText(bytes));
+			
+			Debug.Console(2, this, "Sending {0} bytes: '{1}'", bytes.Length, ComTextHelper.GetEscapedText(bytes));
             if(Client != null)
 			    Client.SendData(bytes, bytes.Length);
 		}
