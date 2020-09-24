fb35f0fc95e79b4176097b509a04714af12261ed
Andrew Welker <andrew.welker@gmail.com>
2020-09-24 16:14:00 -0600

    add RoomId to ControlPropertiesConfig

--- Pepperdash Core/Pepperdash Core/Comm/ControlPropertiesConfig.cs
+++ Pepperdash Core/Pepperdash Core/Comm/ControlPropertiesConfig.cs
@@ -26,6 +26,8 @@ namespace PepperDash.Core
 
         public string IrFile { get; set; }
 
+        public string RoomId { get; set; } //used for VC-4
+
         //public ComPortConfig ComParams { get; set; }
 
         //[JsonConverter(typeof(ComSpecJsonConverter))]
