d129f24c61b298a1786c2068e2c56e74ac55f500
Andrew Welker <awelker@pepperdash.com>
2022-05-20 23:54:26 -0600

    feat: add 4-series support for PepperDash Core

--- Pepperdash Core/Pepperdash Core/Properties/AssemblyInfo.cs
+++ Pepperdash Core/Pepperdash Core/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 [assembly: System.Reflection.AssemblyCompany("PepperDash Technology Corp")]
 [assembly: System.Reflection.AssemblyProduct("Pepperdash_Core")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © PepperDash 2019")]
-[assembly: System.Reflection.AssemblyVersion("1.0.0.*")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersion("0.0.0-buildType-buildNumber")]
 [assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildType-buildNumber")]
