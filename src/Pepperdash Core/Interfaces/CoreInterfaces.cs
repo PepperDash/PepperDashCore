046caba88968aa4145e7b81f908ecb184a91e4ac
Andrew Welker <awelker@pepperdash.com>
2023-03-23 09:25:47 -0600

    chore: rearrange files

---
+++ src/Pepperdash Core/Interfaces/CoreInterfaces.cs
@@ -0,0 +1,24 @@
+﻿namespace PepperDash.Core.Interfaces
+{
+    /// <summary>
+    /// Unique key interface to require a unique key for the class
+    /// </summary>
+	public interface IKeyed
+    {
+        /// <summary>
+        /// Unique Key
+        /// </summary>
+		string Key { get; }
+    }
+
+    /// <summary>
+    /// Named Keyed device interface. Forces the devie to have a Unique Key and a name. 
+    /// </summary>
+	public interface IKeyName : IKeyed
+    {
+        /// <summary>
+        /// Isn't it obvious :)
+        /// </summary>
+		string Name { get; }
+    }
+}
