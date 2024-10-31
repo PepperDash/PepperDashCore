2755377a179c124f4b9d83341e57394ed4a437ad
Nick Genovese <ngenovese@pepperdash.com>
2024-10-31 08:29:25 -0400

    chore: moved some core interfaces into their own files

---
+++ src/Pepperdash.Core/IKeyName.cs
@@ -0,0 +1,15 @@
+﻿namespace PepperDash.Core
+{
+
+    /// <summary>
+    /// Named Keyed device interface. Forces the device to have a Unique Key and a name. 
+    /// </summary>
+	public interface IKeyName : IKeyed
+    {
+        /// <summary>
+        /// Isn't it obvious :)
+        /// </summary>
+		string Name { get; }
+    }
+
+}
---
+++ src/Pepperdash.Core/IKeyed.cs
@@ -0,0 +1,14 @@
+﻿namespace PepperDash.Core
+{
+    /// <summary>
+    /// Unique key interface to require a unique key for the class
+    /// </summary>
+    public interface IKeyed
+	{
+        /// <summary>
+        /// Unique Key
+        /// </summary>
+		string Key { get; }
+    }
+
+}
